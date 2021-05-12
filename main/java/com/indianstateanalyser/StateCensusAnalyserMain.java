767644b5a658fbc3dd9d713fe34f15a1b91eddfc
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-13 00:30:58 +0530

    first commit

---
+++ main/java/com/indianstateanalyser/StateCensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.indianstateanalyser;
+
+public class StateCensusAnalyserMain {
+    public static void main (String[] args) {
+        System.out.println("Indian States Census Analyser Problem");
+    }
+}
