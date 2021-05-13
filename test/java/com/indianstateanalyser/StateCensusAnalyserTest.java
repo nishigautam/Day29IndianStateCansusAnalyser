cf5b74666fb75f1a6c9edcb4595564985b1a87f4
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-13 19:01:04 +0530

    to ensure the number of record matches.

--- test/java/com/indianstateanalyser/StateCensusAnalyserTest.java
+++ test/java/com/indianstateanalyser/StateCensusAnalyserTest.java
@@ -2,10 +2,13 @@ package com.indianstateanalyser;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
 
 public class StateCensusAnalyserTest {
     public static final String CSV_FILE_INDIA_STATE_CENSUS = "C:\\Users\\Nishi\\IdeaProjects\\IndianCensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
-    public static final String INCORRECT_FILE = "C:\\Users\\Nishi\\IdeaProjects\\IndianCensusAnalyser\\src\\main\\resources\\incorrectSample.txt";
+    public static final String INCORRECT_FILE = "./incorrectSample.txt";
     public static final String CSV_FILE_INDIA_STATE_CODE = "C:\\Users\\Nishi\\IdeaProjects\\IndianCensusAnalyser\\src\\main\\resources\\IndiaStateCode";
 
     @Test
@@ -19,4 +22,15 @@ public class StateCensusAnalyserTest {
         }
     }
 
+    @Test
+    public void givenCSVFile_ReturnException_ifFileNotPresent () throws IOException {
+        try {
+            StateCensusAnalyserMain stateCensusAnalyserMain = new StateCensusAnalyserMain();
+            ExpectedException exception = ExpectedException.none();
+            exception.expect(IndianCensusAnalyserException.class);
+            stateCensusAnalyserMain.loadStateCensusData(INCORRECT_FILE);
+        } catch (IndianCensusAnalyserException e) {
+            Assert.assertEquals(IndianCensusAnalyserException.CensusException.CENSUS_FILE_PROBLEM, e.exceptionType);
+        }
+    }
 }
