8a5cdbc7231e6b5a6b022806d1631791b0a17267
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-13 21:31:52 +0530

    Returns custom Exception if StateCensusCSV file is incorrect

--- test/java/com/indianstateanalyser/StateCensusAnalyserTest.java
+++ test/java/com/indianstateanalyser/StateCensusAnalyserTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class StateCensusAnalyserTest {
     public static final String CSV_FILE_INDIA_STATE_CENSUS = "C:\\Users\\Nishi\\IdeaProjects\\IndianCensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
     public static final String INCORRECT_FILE = "./incorrectSample.txt";
+    public static final String INCORRECT_CSV_FILE = "C:\\Users\\Nishi\\IdeaProjects\\IndianCensusAnalyser\\src\\main\\resources\\incorrectSample2.csv";
     public static final String CSV_FILE_INDIA_STATE_CODE = "C:\\Users\\Nishi\\IdeaProjects\\IndianCensusAnalyser\\src\\main\\resources\\IndiaStateCode";
 
     @Test
@@ -23,7 +24,7 @@ public class StateCensusAnalyserTest {
     }
 
     @Test
-    public void givenCSVFile_ReturnException_ifFileNotPresent () throws IOException {
+    public void givenCSVFile_ReturnException_ifFileNotPresent() throws IOException {
         try {
             StateCensusAnalyserMain stateCensusAnalyserMain = new StateCensusAnalyserMain();
             ExpectedException exception = ExpectedException.none();
@@ -31,6 +32,18 @@ public class StateCensusAnalyserTest {
             stateCensusAnalyserMain.loadStateCensusData(INCORRECT_FILE);
         } catch (IndianCensusAnalyserException e) {
             Assert.assertEquals(IndianCensusAnalyserException.CensusException.CENSUS_FILE_PROBLEM, e.exceptionType);
+        }
+    }
+
+    @Test
+    public void givenIncorrectCSVFile_ThrowExceptionForIncorrectCSVFile() {
+        try {
+            StateCensusAnalyserMain stateCensusAnalyserMain = new StateCensusAnalyserMain();
+            ExpectedException exception = ExpectedException.none();
+            exception.expect(IndianCensusAnalyserException.class);
+            stateCensusAnalyserMain.loadStateCensusData(INCORRECT_CSV_FILE);
+        } catch (IndianCensusAnalyserException e) {
+            Assert.assertEquals(IndianCensusAnalyserException.CensusException.INCORRECT_TYPE_ISSUE, e.exceptionType);
         }
     }
 }
